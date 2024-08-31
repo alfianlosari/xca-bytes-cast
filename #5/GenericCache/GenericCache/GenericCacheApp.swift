f376b3449fe75185fb6242885d5b6e6cdec192d5
Alfian Losari <alfianlosari@gmail.com>
2024-08-31 10:42:55 +0700

    Add bytecast # generic cache

---
+++ #5/GenericCache/GenericCache/GenericCacheApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenericCacheApp.swift
+//  GenericCache
+//
+//  Created by Alfian Losari on 31/08/24.
+//
+
+import SwiftUI
+
+@main
+struct GenericCacheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
